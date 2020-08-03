dec0f1b2ce7c566a73e83443b9aa48bed2de3256
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2020-08-03 13:41:45 +0300

    Tile bUiilding system complated

---
+++ Assets/_Scripts/Buiding/BuildingGrid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingGrid : MonoBehaviour
+{
+    public SpriteRenderer alertSprite;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Ground" )
+        {
+            alertSprite.enabled = true;
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Ground" )
+        {
+            alertSprite.enabled = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Ground" )
+        {
+            alertSprite.enabled = false;
+        }
+    }
+}
